7050f6a7c2d943e25dd37031a4c50cd5df4b351a
Sano Bld <117576435+SanoBld@users.noreply.github.com>
2026-08-20 20:26:35 +0200

    refactor: remove unnecessary intent creation in widget tap action

--- android/app/src/main/kotlin/com/example/laststats_mobile/widgets/WidgetUtils.kt
+++ android/app/src/main/kotlin/com/example/laststats_mobile/widgets/WidgetUtils.kt
@@ -5,7 +5,6 @@ package com.example.laststats_mobile.widgets
 
 import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.widget.RemoteViews
 import com.example.laststats_mobile.MainActivity
 import es.antonborri.home_widget.HomeWidgetLaunchIntent
@@ -13,11 +12,7 @@ import es.antonborri.home_widget.HomeWidgetLaunchIntent
 object WidgetUtils {
     // Open the app when the user taps the widget.
     fun setOpenAppIntent(context: Context, views: RemoteViews, layoutId: Int) {
-        val intent: Intent = HomeWidgetLaunchIntent.getActivity(context, MainActivity::class.java)
-        val pending = PendingIntent.getActivity(
-            context, 0, intent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
+        val pending: PendingIntent = HomeWidgetLaunchIntent.getActivity(context, MainActivity::class.java)
         views.setOnClickPendingIntent(layoutId, pending)
     }
 }
