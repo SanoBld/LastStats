26e56d00ab498a5fd4a6fca2bed33002d2759ac3
Sano Bld <117576435+SanoBld@users.noreply.github.com>
2026-08-20 20:16:04 +0200

    refactor: simplify intent creation for widget tap action

--- android/app/src/main/kotlin/com/example/laststats_mobile/widgets/WidgetUtils.kt
+++ android/app/src/main/kotlin/com/example/laststats_mobile/widgets/WidgetUtils.kt
@@ -7,14 +7,13 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
+import com.example.laststats_mobile.MainActivity
 import es.antonborri.home_widget.HomeWidgetLaunchIntent
 
 object WidgetUtils {
     // Open the app when the user taps the widget.
     fun setOpenAppIntent(context: Context, views: RemoteViews, layoutId: Int) {
-        val intent: Intent = HomeWidgetLaunchIntent.getActivity(context, Class.forName(
-            "com.example.laststats_mobile.MainActivity"
-        ))
+        val intent: Intent = HomeWidgetLaunchIntent.getActivity(context, MainActivity::class.java)
         val pending = PendingIntent.getActivity(
             context, 0, intent,
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
